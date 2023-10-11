c36b452a14e47ef117689aac9ed33e1e7179f8cc
bekure27 <bekure27@gmail.com>
2023-10-11 18:59:25 +0300

    update cors

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ app.use(express.json());
 const PORT = 5000;
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "https://amazon-b2t.netlify.app",
+    origin: "http://localhost:5173",
+    // origin: "https://amazon-b2t.netlify.app",
   })
 );
+
+app.use((req, res, next) => {
+  // res.setHeader("Access-Control-Allow-Origin", "https://amazon-b2t.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", " http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 // console.log(process.env.STRIPE_PRIVATE_KEY);
@@ -24,7 +34,7 @@ app.post("/payments/create", async (request, response) => {
   // const total = Math.round(request.query.total * 100);
 
   const total = request.query.total;
-
+if (total ){
   console.log("total amount ", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
@@ -35,6 +45,14 @@ app.post("/payments/create", async (request, response) => {
   response.status(201).send({
     clientSecret: paymentIntent.client_secret,
   });
+
+}
+else {
+    response.status(200).send({
+      message: "i'cant process payment ",
+    });
+}
+
 });
 
 app.listen(PORT, () => {
